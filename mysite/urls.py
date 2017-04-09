ac99717f752ae7455e41b97b47fc8b9d64f6f7ee
brian <brianbui90@gmail.com>
2017-04-09 14:02:03 -0700

    added view and template

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
-	url(r'^post/new/$', views.post_new, name='post_new'),
-	 url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
